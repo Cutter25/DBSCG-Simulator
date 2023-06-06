012f4bb8021c430cdd8b1a59504de9b05ed6fac1
Corbin Cutter <corbinc03123@gmail.com>
2023-06-06 10:30:46 -0400

    Header

--- dbscg-simulator-react-app/src/App.js
+++ dbscg-simulator-react-app/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Greet from './components/Greet';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Greet />
+      <Header />
       // The above tag is "Self-Closing!"
     </div>
   );
