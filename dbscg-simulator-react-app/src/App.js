ef2846b2fad780dd2c254060da4fbcd4c645e938
Corbin Cutter <corbinc03123@gmail.com>
2023-06-06 00:14:33 -0400

    Understanding React

--- dbscg-simulator-react-app/src/App.js
+++ dbscg-simulator-react-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      
+      <Greet />
+      // The above tag is "Self-Closing!"
     </div>
   );
 }
